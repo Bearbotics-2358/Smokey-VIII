67abf971021ded197e00b60a43251d747857af94
Alexander Meyer <alexander.dude.meyer@gmail.com>
2015-03-25 20:58:51 -0500

    Untested switch to zmqpp (only in RemoteLightRing at the moment).

--- src/RemoteLightRing.h
+++ src/RemoteLightRing.h
@@ -1,14 +1,15 @@
 #pragma once
 
+#include <zmqpp.hpp>
+
 class RemoteLightRing {
 public:
-  RemoteLightRing();
-  RemoteLightRing(void *zmqCtx);
+  RemoteLightRing(zmqpp::context &ctx);
   ~RemoteLightRing();
 
   void SetColor(int device, int r, int g, int b);
 
 private:
-  void *_zmqCtx;
-  void *_zmqSocket;
+  zmqpp::context &_ctx;
+  zmqpp::socket _socket;
 };
