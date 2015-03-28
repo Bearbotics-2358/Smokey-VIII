a9bf4ae8929430d329cf0b6c8c014eee5ff215d6
bearbotics <bearbotics2358@gmail.com>
2015-03-28 11:19:32 -0500

    Roller code should actually compile now.

--- src/Smokey_VIII.cpp
+++ src/Smokey_VIII.cpp
@@ -21,7 +21,7 @@ Smokey_VIII::Smokey_VIII(void)
   // a_PDP(),
   a_DriveEncoder(DRIVE_ENCODER_PORT_1, DRIVE_ENCODER_PORT_2, true, Encoder::k4X),
   a_AutonTimer(),
-  a_AutonState(kGrabbing)
+  a_AutonState(kGrabbing),
   a_Rollers()
 {
 	a_Drive.SetInvertedMotor(a_Drive.kRearRightMotor, true);
